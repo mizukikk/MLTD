01da8f06514e22e1f4ef092c4fde883727e55048
kevin <Kevin@ddrift.com>
2020-08-26 15:26:38 +0800

    change repository single instance

--- app/src/main/java/com/mizukikk/mltd/Inject.kt
+++ app/src/main/java/com/mizukikk/mltd/Inject.kt
@@ -1,9 +1,11 @@
 package com.mizukikk.mltd
 
 import com.mizukikk.mltd.data.MLTDRepository
+import com.mizukikk.mltd.data.source.remote.MLTDRemoteDataSource
 
 object Inject {
 
-    fun providerMLTDRepository() = MLTDRepository()
+    fun providerMLTDRepository() =
+        MLTDRepository.newInstance(MLTDRemoteDataSource)
 
 }
--- app/src/main/java/com/mizukikk/mltd/data/MLTDRepository.kt
+++ app/src/main/java/com/mizukikk/mltd/data/MLTDRepository.kt
@@ -5,9 +5,16 @@ import com.mizukikk.mltd.api.response.Event
 import com.mizukikk.mltd.data.source.remote.MLTDRemoteDataSource
 import com.mizukikk.mltd.data.source.remote.RemoteDataSource
 
-class MLTDRepository : RemoteDataSource {
+class MLTDRepository private constructor(private val remoteDataSource: RemoteDataSource) :
+    RemoteDataSource {
 
-    private val remoteDataSource = MLTDRemoteDataSource
+    companion object {
+        private var INSTANCE: MLTDRepository? = null
+        fun newInstance(remoteDataSource: RemoteDataSource) =
+            INSTANCE ?: synchronized(MLTDRemoteDataSource) {
+                MLTDRepository(remoteDataSource)
+            }
+    }
 
     override fun getAllEvent(callBack: ResponseCallBack<List<Event.EventResponse>>) {
         remoteDataSource.getAllEvent(callBack)
