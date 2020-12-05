b72e943aed7af652b05e27a20b82e3d8d121c0c0
mizukikk <k87402@hotmail.com>
2020-12-05 13:08:50 +0800

    remove picture fullscreen systemUiVisibility settings

--- app/src/main/java/com/mizukikk/mltd/picture/PictureActivity.kt
+++ app/src/main/java/com/mizukikk/mltd/picture/PictureActivity.kt
@@ -60,14 +60,6 @@ class PictureActivity : AppCompatActivity() {
     private val savePhotoSizeArray by lazy { resources.getStringArray(R.array.savePhotoSize) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.decorView.systemUiVisibility =
-            (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_picture)
         checkIntentExtra()
